ffc930de69a18898a0dc39918acd7ee27801232c
Andrey <andrejsatykov@yandex.ru>
2022-12-11 10:33:36 +0300

    Demonstration of the solution Task 01

---
+++ Task_01/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, замена элементов массива положительные заменить на
+// отрицательные и соответственно наоборот.
+Console.Clear();
+
+
+int[] FillArreyWithRandomNumbers(int size, int leftRange, int rightRange)
+{
+    Random rand = new Random(); // заполним его случайными числами, подключаем 
+                                // объект Random с названием rand c классом new Random.
+    int[] arr = new int[size] ;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
+}
+
